3ddf17267bac6677f4f8b69166e60a3eba15cd49
Abhishek Singh Adhikari <adhikariabhishek290@gmail.com>
2021-06-22 01:12:55 +0530

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int addition(int, int);
+
+int main(){
+
+    int a,b;
+    cout<<"Enter input a and b: ";
+    cin>>a>>b;
+
+    cout<<"\nAnswer is: "<< addition(a,b);
+    getchar();
+
+    return 0;
+}
