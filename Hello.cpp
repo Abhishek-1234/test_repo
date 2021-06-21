f0e45352a4d07a454ab104b14fafefac5964e480
Abhishek Singh Adhikari <adhikariabhishek290@gmail.com>
2021-06-22 01:13:52 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,15 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-int addition(int, int);
 
 int main(){
 
-    int a,b;
-    cout<<"Enter input a and b: ";
-    cin>>a>>b;
-
-    cout<<"\nAnswer is: "<< addition(a,b);
-    getchar();
-
+    cout<<"Hello cpp world.";
+    
     return 0;
 }
